f7f1a60b59a5fc1b55a546da380757d344264e84
Debayanpaul1593 <debayan.sec@gmail.com>
2021-03-02 23:15:35 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <View style={{flex: 1}}>
+      <Text>Hello!</Text>
+    </View>
+  );
+};
+
+export default App;
